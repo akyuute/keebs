2e4828fde24733a7d3a796ccd6d4be2372719017
Samantha <trackmenot10@gmail.com>
2025-04-17 21:46:45 -0700

    This should work in theory, but as usual, nothing happens when I flash

--- keyboards/royal_kludge/rk61/keymaps/vimwin/keymap.c
+++ keyboards/royal_kludge/rk61/keymaps/vimwin/keymap.c
@@ -5,8 +5,8 @@ enum {
     _SYMB,
     _MVMT,
     _MOUSE,
-    _NUMPAD,
-    _SPEC,
+    // _NUMPAD,
+    // _SPEC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -24,16 +24,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_GRV, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL, KC_PLUS, KC_PIPE, KC_TRNS,
         KC_LALT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DQUO, KC_TRNS,KC_TRNS,
         KC_EXLM, KC_COLN, KC_LCTL, KC_LSFT, KC_LGUI, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,KC_TRNS,KC_TRNS,
-        KC_TRNS, MO(_SPEC), KC_ENT, KC_TRNS, KC_DEL, KC_RALT, KC_RCTL, KC_TRNS
+        KC_TRNS, MO(0), KC_ENT, KC_TRNS, KC_DEL, KC_RALT, KC_RCTL, KC_TRNS
     ),
 
     [_MVMT] = LAYOUT_all(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,KC_NO,
         // KC_SLEP,  KC_BRID,  KC_BRIU,  KC_NO,  KC_NO,  KC_NO,  KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_NO,KC_NO,
         KC_APP, LCTL(KC_Z), LCTL(KC_Y), KC_F2, KC_F5, KC_NO, KC_SPC, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_BSPC, KC_DEL, KC_NO,
-        KC_LALT, LCTL(KC_A), LCTL(KC_C), LCTL(KC_V), LCTL(KC_X), KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_ENT,
+        KC_LALT, LCTL(KC_A), LCTL(KC_C), LCTL(KC_V), LCTL(KC_X), KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_ENT, KC_ENT,
         KC_LSFT, KC_LGUI, KC_LSFT, KC_LCTL, KC_TAB, LSFT(KC_TAB), KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_RSFT,KC_NO,KC_NO,
-        KC_LCTL, KC_TRNS, KC_LALT, MO(_SPEC), KC_DEL, KC_TRNS, KC_APP, KC_NO
+        KC_LCTL, KC_TRNS, KC_LALT, MO(0), KC_DEL, KC_TRNS, KC_APP, KC_NO
     ),
 
     [_MOUSE] = LAYOUT_all(
@@ -44,6 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_APP, KC_SPC, MS_BTN1, MS_BTN2, MS_ACL2, KC_TRNS
     ),
 
+    /*
     [_NUMPAD] = LAYOUT_all(
         TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_ASTR, KC_BSPC,KC_NO,
         KC_TAB, KC_PGUP, KC_UP, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_MINS, KC_PLUS, KC_TAB,
@@ -53,21 +54,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SPEC] = LAYOUT_all(
-/*
  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,_______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_VAD,  RGB_VAI,  RGB_MOD,          // RGB not yet supported
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_SPD,  RGB_SPI,  RGB_TOG,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_SAD,  RGB_SAI,  RGB_HUI,  _______,  _______,_______,_______,
     _______,      _______,      _______,      _______, 	                                           _______,      _______,      _______,     _______
-*/
 
-        TO(_NUMPAD), _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,_______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(_NUMPAD),  _______,  _______,  _______,  _______,
+        TO(0), _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,_______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(0),  _______,  _______,  _______,  _______,
         QK_BOOT,  _______,  KC_SLEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_SLEP,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  BL_DOWN,  BL_UP,    _______,  _______,_______,_______,
         _______,      _______,      _______,      _______, 	                                           _______,      _______,      _______,     _______
 
     )
+    */
+/*
+*/
 
 };
 
